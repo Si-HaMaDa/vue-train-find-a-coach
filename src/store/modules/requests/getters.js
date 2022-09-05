7c44a0f74baf1310cbabc2c8a00bbd9cf5ef5c5b
Mohamed <nrookman_27@yahoo.com>
2022-09-05 22:03:49 +0200

    coache requests

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+    requests(state, _, _2, rootGetters) {
+        const coachId = rootGetters.userId;
+        return state.requests.filter((req) => req.coachId === coachId);
+    },
+    authRequests(_, getters, _2, rootGetters) {
+        const coachId = rootGetters.userId;
+        return getters.requests.filter((req) => req.coachId === coachId);
+    },
+    hasRequests(_, getters) {
+        return getters.authRequests && getters.authRequests.length > 0;
+    },
+};
